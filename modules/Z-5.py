567b9edd6ae21223358cb99d30ccf368ec925c0a
MaximOlifer <maximolifer00@gmail.com>
2019-03-12 21:29:25 +0300

    Created classes.py and added Z-5 module

---
+++ modules/Z-5.py
@@ -0,0 +1,8 @@
+import classes
+
+
+def TRANS_Z_N (x):
+    ans = None
+    if x.b == 0:
+        ans = Natural(x.n, *[x[i] for i in range(n)])
+    return ans
---
+++ modules/classes.py
@@ -0,0 +1,22 @@
+class Natural (list):
+    def __init__ (self, n = 0, *args):
+        self.n = n
+        super().__init__(args)
+        
+
+class Integer(Natural):
+    def __init__(self, b = 0, n = 0, *args):
+        self.b = b
+        super().__init__(n, args)
+
+
+class Rational:
+    def __init__(self, m=Integer(), n=Natural(1,1)):
+        self.m = m
+        self.n = n
+        
+
+class Polinomial (list):
+    def __init__(self, m=0, *args):
+        self.m = m
+        super().__init__(*args)
